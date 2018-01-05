78db8814f21756393a79298ccf8fcf47c7e35e85
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-01-05 14:45:27 +0800

    (MODULES-6362) Add acceptance tests for taskscheduler_api2 provider
    Previously the acceptance tests were for vanilla beaker.  As this is now a
    module, we use beaker-rspec.  This commit converts the create and destroy tests
    to beaker-rspec and removes the query and modify tests as they are not required.

---
+++ spec/acceptance/should_destroy_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper_acceptance'
+
+host = find_only_one("default")
+
+describe "Should destroy a scheduled task", :node => host do
+
+  before(:all) do
+    @taskname = "pl#{rand(999999).to_i}"
+
+    pp = <<-MANIFEST
+    scheduled_task {'#{@taskname}':
+      ensure      => present,
+      command     => 'c:\\\\windows\\\\system32\\\\notepad.exe',
+      arguments   => "foo bar baz",
+      working_dir => 'c:\\\\windows',
+      trigger     => {
+        schedule   => daily,
+        start_time => '12:00',
+      },
+      provider => 'taskscheduler_api2'
+    }
+    MANIFEST
+    execute_manifest_on(host, pp, :catch_failures => true)
+  end
+
+  it 'Should destroy the task' do
+    # Verify the task exists
+    query_cmd = "schtasks.exe /query /v /fo list /tn #{@taskname}"
+    on(host, query_cmd)
+
+    pp = <<-MANIFEST
+    scheduled_task {'#{@taskname}':
+      ensure      => absent,
+      provider    => 'taskscheduler_api2'
+    }
+    MANIFEST
+
+    execute_manifest_on(host, pp, :catch_failures => true)
+
+    query_cmd = "schtasks.exe /query /v /fo list /tn #{@taskname}"
+    query_out = on(host, query_cmd, :accept_all_exit_codes => true).output
+    expect(query_out).to eq("ERROR: The system cannot find the file specified.\n\n")
+  end
+end
