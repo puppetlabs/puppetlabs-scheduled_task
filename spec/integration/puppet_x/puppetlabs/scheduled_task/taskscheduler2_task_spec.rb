35a8c6a38d966434ecd88c700f747afde6901b81
Michael T Lombardi <michael.lombardi@puppet.com>
2018-03-15 14:08:26 -0700

    Add CRUD tests for new helper

--- spec/integration/puppet_x/puppetlabs/scheduled_task/taskscheduler2_task_spec.rb
+++ spec/integration/puppet_x/puppetlabs/scheduled_task/taskscheduler2_task_spec.rb
@@ -5,6 +5,27 @@ require 'puppet/util/windows/taskscheduler' if Puppet.features.microsoft_windows
 require 'puppet_x/puppetlabs/scheduled_task/taskscheduler2_task'
 # require 'puppet_x/puppetlabs/scheduled_task/taskscheduler2'
 
+RSpec::Matchers.define :be_same_as_powershell_command do |ps_cmd|
+  define_method :run_ps do |cmd|
+    full_cmd = "powershell.exe -NoLogo -NoProfile -NonInteractive -Command \"#{cmd}\""
+
+    result = `#{full_cmd}`
+
+    result.strip
+  end
+
+  match do |actual|
+    from_ps = run_ps(ps_cmd)
+
+    # This matcher probably won't tolerate UTF8 characters
+    actual.to_s == from_ps
+  end
+
+  failure_message do |actual|
+    "expected that #{actual} would match #{run_ps(ps_cmd)} from PowerShell command #{ps_cmd}"
+  end
+end
+
 def triggers
   now = Time.now
 
@@ -24,17 +45,10 @@ def triggers
   [
     # dummy time trigger
     defaults.merge({
-      'random_minutes_interval' => 0,
+      'random_minutes_interval' => 1,
       'trigger_type'            => :TASK_TRIGGER_TIME,
     }),
     defaults.merge({
-      # TODO: should this be settable?
-      # 'random_minutes_interval' => 1,
-      'trigger_type'            => :TASK_TRIGGER_TIME,
-    }),
-    defaults.merge({
-      # TODO: should this be settable?
-      # 'random_minutes_interval' => 1,
       'type'                    => { 'days_interval' => 1 },
       'trigger_type'            => :TASK_TRIGGER_DAILY,
     }),
@@ -87,6 +101,10 @@ describe PuppetX::PuppetLabs::ScheduledTask::TaskScheduler2Task, :if => Puppet.f
         task.save
       end
 
+      after(:each) do
+        subject.delete(@task_name) if subject.exists?(@task_name)
+      end
+
       # TODO: test each of the currently supported trigger types
       # v2 API is weird and prefers to have '' for RandomDelay for TASK_TRIGGER_TIME
       it 'and should return the same properties as those set' do
@@ -97,21 +115,90 @@ describe PuppetX::PuppetLabs::ScheduledTask::TaskScheduler2Task, :if => Puppet.f
         expect(subject.parameters).to eq('/c exit 0')
         expect(subject.application_name).to eq('cmd.exe')
         expect(subject.trigger_count).to eq(1)
-
-
-        # require 'pry'; binding.pry
-        # TODO: need to compare to dummy_trigger
-        # require 'pry'; binding.pry
-        # expect(subject.trigger(0).Flags).to eq(trigger['flags'])
-      end
-
-      after(:each) do
-        subject.delete(@task_name) if subject.exists?(@task_name)
+        expect(subject.trigger(0)['trigger_type']).to eq(trigger['trigger_type'])
+        expect(subject.trigger(0)['type']).to eq(trigger['type']) if trigger['type']
+        expect(task.Definition.Triggers.Item(1).RandomDelay).to eq("PT#{trigger['random_minutes_interval']}M") if trigger['random_minutes_interval']
+                
       end
     end
   end
 
+  context "When managing a task" do
+    before(:each) do
+      @task_name = 'puppet_task_' + SecureRandom.uuid.to_s
+      task = subject.class.new(@task_name, triggers[0])
+      task.application_name = 'cmd.exe'
+      task.parameters = '/c exit 0'
+      task.save
+    end
 
+    after(:each) do
+      subject.delete(@task_name) if subject.exists?(@task_name)
+    end
+
+    it 'should be able to determine if the task exists or not' do
+      bad_task_name = SecureRandom.uuid.to_s
+      expect(subject.exists?(@task_name)).to be(true)
+      expect(subject.exists?(bad_task_name)).to be(false)
+    end
+
+    it 'should able to update a trigger' do
+      new_trigger = triggers[0].merge({
+        'start_year'              => 2112,
+        'start_month'             => 12,
+        'start_day'               => 12,
+        'start_hour'              => 12,
+        'start_minute'            => 12,
+      })
+
+      task = subject.activate(@task_name)
+      expect(subject.delete_trigger(0)).to be(1)
+      subject.trigger = new_trigger
+      subject.save
+      ps_cmd = '[string]((Get-ScheduledTask | ? { $_.TaskName -eq \'' + @task_name + '\' }).Triggers.StartBoundary)'
+      expect('2112-12-12T12:12:00').to be_same_as_powershell_command(ps_cmd)
+    end
+
+    it 'should be able to update a command' do
+      new_application_name = 'notepad.exe'
+      ps_cmd = '[string]((Get-ScheduledTask | ? { $_.TaskName -eq \'' + @task_name + '\' }).Actions[0].Execute)'
+      task = subject.activate(@task_name)
+
+      expect('cmd.exe').to be_same_as_powershell_command(ps_cmd)
+
+      subject.application_name = new_application_name
+      subject.save
+      
+      expect(new_application_name).to be_same_as_powershell_command(ps_cmd)
+    end
+
+    it 'should be able to update command parameters' do
+      new_parameters = '/nonsense /utter /nonsense'
+      ps_cmd = '[string]((Get-ScheduledTask | ? { $_.TaskName -eq \'' + @task_name + '\' }).Actions[0].Arguments)'
+      task = subject.activate(@task_name)
+
+      expect('/c exit 0').to be_same_as_powershell_command(ps_cmd)
+
+      subject.parameters = new_parameters
+      subject.save
+      
+      expect(new_parameters).to be_same_as_powershell_command(ps_cmd)
+    end
+
+    it 'should be able to update the working directory' do
+      new_working_directory = 'C:\Somewhere'
+      ps_cmd = '[string]((Get-ScheduledTask | ? { $_.TaskName -eq \'' + @task_name + '\' }).Actions[0].WorkingDirectory)'
+      task = subject.activate(@task_name)
+
+      expect('').to be_same_as_powershell_command(ps_cmd)
+
+      subject.working_directory = new_working_directory
+      subject.save
+      
+      expect(new_working_directory).to be_same_as_powershell_command(ps_cmd)
+    end
+  end
+  
   # TODO: this test is kind of pointless right now
   # it 'should be mutable with V2 API' do
   #   arguments_after = '/c exit 255'
