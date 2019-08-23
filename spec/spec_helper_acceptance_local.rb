5c94c1a712f6549ead77e2315ab88861c3c8c0ab
tphoney <tp@puppet.com>
2019-08-23 12:02:41 +0100

    (maint) move helpers into acceptance_local

---
+++ spec/spec_helper_acceptance_local.rb
@@ -0,0 +1,17 @@
+def add_test_user
+  username = "test_user_#{rand(999).to_i}"
+  password = 'password!@#123'
+
+  command_string = "net user /add #{username} #{password}"
+  run_shell(command_string) do |r|
+    raise r.stderr unless r.stderr.empty?
+  end
+  [username, password]
+end
+
+def remove_test_user(username)
+  command_string = "net user /delete #{username}"
+  run_shell(command_string) do |r|
+    raise r.stderr unless r.stderr.empty?
+  end
+end
