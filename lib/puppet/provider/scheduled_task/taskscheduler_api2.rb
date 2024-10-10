ac1a4791ac660097cc3f6708e61360e4ccd4a37f
voreilly <vernon.oreilly@gmail.com>
2024-10-10 18:33:09 +1100

    Update taskscheduler_api2.rb
    Update legacy fact

--- lib/puppet/provider/scheduled_task/taskscheduler_api2.rb
+++ lib/puppet/provider/scheduled_task/taskscheduler_api2.rb
@@ -8,8 +8,8 @@ Puppet::Type.type(:scheduled_task).provide(:taskscheduler_api2) do
        This is a technical preview using the newer V2 API interface but
        still editing V1 compatbile scheduled tasks."
 
-  defaultfor operatingsystem: :windows
-  confine    operatingsystem: :windows
+  defaultfor 'os.name': :windows
+  confine    'os.name': :windows
 
   has_feature :compatibility
 
